7d6057eb70ad16542ca77664d99bb3786db8b74c
Gilberto Arzola <gilbertoarz14@gmail.com>
2023-09-08 23:09:08 -0600

    Segundo commit

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const bot = new Telegraf(token)
 
 bot.start((ctx) => ctx.reply("Porque eres tan puto ricky?"));
 
+bot.help((ctx) => ctx.reply("Aqui puedes ver los detalles"))
+
+bot.command("empezar" , (ctx) => ctx.reply("Cuanto vendiste?"))
+
+
+
+bot.hears('1', (ctx) => ctx.reply("escogiste el 1"))
+
+bot.on("text", (ctx) => {
+    let mensaje = ctx.message.text;
+    ctx.reply(mensaje)
+})
+
+
 
 console.log ("El bot esta activo")
 
